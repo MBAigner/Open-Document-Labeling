9ef288f0f5c4243728384f1b9f49cbeaf0ac865c
Michael Aigner <Michael.Aigner-CIC@ibm.com>
2020-07-23 15:19:44 +0200

    add implementation and example documents

---
+++ main/remove_output.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+rm -rf output/.tracker/*
+rm -rf output/PASCAL_VOC/*
+rm -rf output/Real_data/*
+rm -rf output/YOLO_darknet/*
+rm -rf input/*
+
+echo "Finished removing all the outputs"
---
+++ util/constants.py
@@ -0,0 +1,6 @@
+
+LABEL_INPUT_PATH = "../main/input/"
+PDF_LABEL_INPUT_PATH = "../data/custom/pdfs/"
+LABEL_OUTPUT_PATH = "../main/output/YOLO_darknet/"
+LABELED_DATA_FILE_PATH = PDF_LABEL_INPUT_PATH
+LABELED_DATA_FILE_EXT = "pdf"
